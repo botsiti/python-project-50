6e750b735ce659b6b9fbce411aef67134aa9781e
batraz <batrazbotsiev@gmail.com>
2023-03-13 18:53:04 +0300

    gendiff step1 help output

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
