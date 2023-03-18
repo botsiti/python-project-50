8b13db57e7a652f236eccb5f751c0886d47de7dd
batraz <batrazbotsiev@gmail.com>
2023-03-18 12:59:11 +0300

    engine beta

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,2 +1,3 @@
 from gendiff.scripts.gendiff_gen import generate_diff
-__all__ = ('generate_diff')
+from gendiff.scripts.cli import parser, args
+__all__ = ('generate_diff', 'parser', 'args')
---
+++ gendiff/scripts/engine_gendiff.py
@@ -0,0 +1,12 @@
+from gendiff import parser, args
+from gendiff import generate_diff
+
+parser
+
+def main():
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
+
+
+if __name__ == '__main__':
+    main()
