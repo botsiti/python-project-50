f7f654825db2aa15c331b50897b0242b2d01e0d3
batraz <batrazbotsiev@gmail.com>
2023-03-21 13:22:21 +0300

    step 4 bug fix

--- Makefile
+++ Makefile
@@ -3,3 +3,20 @@ install:
 
 lint:
 	poetry run flake8 gendiff
+
+test:
+	poetry run pytest
+
+test-coverage:
+	poetry run pytest --cov=gendiff --cov-report xml
+
+
+selfcheck:
+	poetry check
+
+check: selfcheck test lint
+
+build: check
+	poetry build
+
+.PHONY: install test lint selfcheck check build
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,8 +1,11 @@
-from gendiff import generate_diff
+from gendiff.scripts.gendiff_gen import generate_diff
 
 
-def test_gendiff():
-    fl1 = 'fixtures/file1.json'
-    fl2 = 'fixtures/file2.json'
-    result = 'fixtures/test1_result'
-    assert generate_diff(fl1, fl2) == result
+fl1 = 'tests/fixtures/file1.json'
+fl2 = 'tests/fixtures/file2.json'
+result1 = 'tests/fixtures/result1.txt'
+read_result1 = open(result1, 'r')
+
+
+def test_gendiff_flat():
+    assert generate_diff(fl1, fl2) == read_result1.read()
