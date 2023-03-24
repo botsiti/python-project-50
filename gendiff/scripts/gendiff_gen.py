fd730c3d8b55034cc45ab13c62a71fcbdb111798
batraz <batrazbotsiev@gmail.com>
2023-03-24 09:47:38 +0300

    add raise valueerror

--- gendiff/scripts/gendiff_gen.py
+++ gendiff/scripts/gendiff_gen.py
@@ -6,12 +6,17 @@ import yaml
 def file_opener(file1, file2):
     if file1.endswith('.json'):
         file1 = json.load(open(file1))
-    else:
+    elif file1.endswith('.yaml') or file1.endswith('yml'):
         file1 = yaml.safe_load(open(file1, 'r'))
+    else:
+        raise ValueError('Invalid format')
+
     if file2.endswith('.json'):
         file2 = json.load(open(file2))
-    else:
+    elif file2.endswith('yaml') or file2.endswith('yml'):
         file2 = yaml.safe_load(open(file2, 'r'))
+    else:
+        raise ValueError('Invalid format')
     return file1, file2
 
 
